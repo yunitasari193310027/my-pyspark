e7c2803ee54c54a6df9a1a2bcc155786435bf95f
yunitasari193310027 <ynt.yunitasari@gmail.com>
2023-05-10 11:27:47 +0700

    fic space&tab

--- payr2_nominimalisir.py
+++ payr2_nominimalisir.py
@@ -24,13 +24,13 @@ df_MTD = df_MTD.select('subs_id_mtd','lacci_id_mtd')
 df_MTD = df_MTD.dropDuplicates(['subs_id_mtd'])
 
 # JOIN ke MTD
-dfJOIN_MTD = dfPAYR_SUB.join(df_MTD,dfPAYR_SUB.subs_id ==  df_MTD.subs_id_mtd, how = 'left')
+dfJOIN_MTD = dfPAYR_SUB.join(df_MTD,dfPAYR_SUB.subs_id == df_MTD.subs_id_mtd, how = 'left')
 dfJOIN_MTD = dfJOIN_MTD.withColumn("trx_lacci", when(dfJOIN_MTD.trx_lacci == "|","").otherwise(dfJOIN_MTD.trx_lacci))
 
 # SWITCH CASE trx_lacci
-dfJOIN_MTD = dfJOIN_MTD.withColumn('trx_lacci', 
-                      F.when((F.col('trx_lacci').isNull()) & (F.col('subs_id') == F.col('subs_id_mtd')), F.col('lacci_id_mtd'))
-                          .otherwise(F.col('trx_lacci')))
+dfJOIN_MTD = dfJOIN_MTD.withColumn('trx_lacci',
+                F.when((F.col('trx_lacci').isNull()) & (F.col('subs_id') == F.col('subs_id_mtd')), F.col('lacci_id_mtd'))
+                    .otherwise(F.col('trx_lacci')))
 
 # READ DATA MM
 df_MM = spark.read.parquet("file:///home/hdoop/datayunita/dom_mm")
@@ -39,17 +39,17 @@ df_MM = df_MM.select('subs_id_mm','lacci_id_mm')
 df_MM = df_MM.dropDuplicates(['subs_id_mm'])
 
 # JOIN ke MM
-dfJOIN_MM = dfJOIN_MTD.join(df_MM,dfJOIN_MTD.subs_id ==  df_MM.subs_id_mm, how = 'left')
+dfJOIN_MM = dfJOIN_MTD.join(df_MM,dfJOIN_MTD.subs_id == df_MM.subs_id_mm, how = 'left')
 
 # SWITCH CASE trx_lacci
 dfJOIN_MM = dfJOIN_MM.withColumn('trx_lacci', 
-                      F.when((F.col('trx_lacci').isNull()) & (F.col('subs_id') == F.col('subs_id_mm')), F.col('lacci_id_mm'))
-                          .otherwise(F.col('trx_lacci')))
+                    F.when((F.col('trx_lacci').isNull()) & (F.col('subs_id') == F.col('subs_id_mm')), F.col('lacci_id_mm'))
+                        .otherwise(F.col('trx_lacci')))
 
 dfJOIN_MM = dfJOIN_MM.withColumn('find_flag', 
-                      F.when((F.col('subs_id') == F.col('subs_id_mtd')) & (F.col('lacci_id_mtd') == F.col('trx_lacci')), '1#')
-                        .when((F.col('subs_id') == F.col('subs_id_mm')) & (F.col('lacci_id_mm') == F.col('trx_lacci')), '2#')
-                          .otherwise("0"))
+                    F.when((F.col('subs_id') == F.col('subs_id_mtd')) & (F.col('lacci_id_mtd') == F.col('trx_lacci')), '1#')
+                    .when((F.col('subs_id') == F.col('subs_id_mm')) & (F.col('lacci_id_mm') == F.col('trx_lacci')), '2#')
+                        .otherwise("0"))
 
 dfJOIN_MM = dfJOIN_MM.fillna("-99",["trx_lacci"])
 
@@ -59,35 +59,35 @@ df_LACCIMA = df_LACCIMA.select('lacci_id','eci','cgi_post','area_sales','lac','c
 df_LACCIMA = df_LACCIMA.toDF('lacci_id_laccima','eci','cgi_post','area_name','lac','ci','node_type','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan')
 
 # JOIN ke LACCI_ID LACCIMA
-dfJOIN_LACCI = dfJOIN_MM.join(df_LACCIMA,dfJOIN_MM.trx_lacci ==  df_LACCIMA.lacci_id_laccima, how = 'left')
+dfJOIN_LACCI = dfJOIN_MM.join(df_LACCIMA,dfJOIN_MM.trx_lacci == df_LACCIMA.lacci_id_laccima, how = 'left')
 dfJOIN_LACCI = dfJOIN_LACCI.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','lacci_id_laccima','eci','cgi_post','area_name','lac','ci','node_type','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','find_flag')
 
 # CREATE FLAG for split filter
 data_flagLACCI = dfJOIN_LACCI.withColumn('Flag',when(col('trx_lacci') == col('lacci_id_laccima'),'SAME').otherwise('DIFF'))
-df_goodLACCI  = data_flagLACCI.filter(col('Flag')  == 'SAME')
+df_goodLACCI = data_flagLACCI.filter(col('Flag') == 'SAME')
 df_rejectLACCI = data_flagLACCI.filter(col('Flag') == 'DIFF')
-df_goodLACCI  = df_goodLACCI.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','lacci_id_laccima','eci','cgi_post','area_name','lac','ci','node_type','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','find_flag') 
+df_goodLACCI = df_goodLACCI.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','lacci_id_laccima','eci','cgi_post','area_name','lac','ci','node_type','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','find_flag') 
 df_rejectLACCI = df_rejectLACCI.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','find_flag')
 
 #JOIN ke ECI LACCIMA
-dfJOIN_ECI = df_rejectLACCI.join(df_LACCIMA,df_rejectLACCI.trx_lacci ==  df_LACCIMA.eci, how = 'left')
+dfJOIN_ECI = df_rejectLACCI.join(df_LACCIMA,df_rejectLACCI.trx_lacci == df_LACCIMA.eci, how = 'left')
 dfJOIN_ECI = dfJOIN_ECI.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','lacci_id_laccima','eci','cgi_post','area_name','lac','ci','node_type','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','find_flag')
 
 # CREATE FLAG for split filter
 data_flagECI = dfJOIN_ECI.withColumn('Flag',when(col('trx_lacci') == col('eci'),'SAME').otherwise('DIFF'))
-df_goodECI = data_flagECI.filter(col('Flag')  == 'SAME')
+df_goodECI = data_flagECI.filter(col('Flag') == 'SAME')
 df_rejectECI = data_flagECI.filter(col('Flag') == 'DIFF')
 df_goodECI = df_goodECI.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','lacci_id_laccima','eci','cgi_post','area_name','lac','ci','node_type','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','find_flag')
 df_rejectECI = df_rejectECI.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','find_flag')
 
 # JOIN ke cgi_post LACCIMA
 df_LACCIMA = df_LACCIMA.dropDuplicates(['cgi_post'])
-dfJOIN_CGI = df_rejectECI.join(df_LACCIMA,df_rejectECI.trx_lacci ==  df_LACCIMA.cgi_post, how = 'left')
+dfJOIN_CGI = df_rejectECI.join(df_LACCIMA,df_rejectECI.trx_lacci == df_LACCIMA.cgi_post, how = 'left')
 df_CGI = dfJOIN_CGI.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','lacci_id_laccima','eci','cgi_post','area_name','lac','ci','node_type','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','find_flag')
 
 # CREATE FLAG for split filter to cek good&reject cgi 
 data_flagCGI = dfJOIN_CGI.withColumn('Flag',when(col('trx_lacci') == col('cgi_post'),'SAME').otherwise('DIFF'))
-df_goodCGI = data_flagCGI.filter(col('Flag')  == 'SAME')
+df_goodCGI = data_flagCGI.filter(col('Flag') == 'SAME')
 df_rejectCGI = data_flagCGI.filter(col('Flag') == 'DIFF')
 # CEK
 #print("Total row hasil Join CGI good=",df_goodCGI.count(),"dan reject=",df_rejectCGI.count())
@@ -100,18 +100,18 @@ dfMerge = reduce(DataFrame.unionAll, df)
 
 # SWITCH CASE lacci_id
 dfMerge = dfMerge.withColumn('flag', 
-                      F.when((F.col('trx_lacci') == F.col('lacci_id_laccima')) | (F.col('trx_lacci') == F.col('eci')) | (F.col('trx_lacci') == F.col('cgi_post')), "y")
-                          .otherwise("x"))
+                    F.when((F.col('trx_lacci') == F.col('lacci_id_laccima')) | (F.col('trx_lacci') == F.col('eci')) | (F.col('trx_lacci') == F.col('cgi_post')), "y")
+                        .otherwise("x"))
 
 df1 = dfMerge.withColumn('lacci_id_flag', 
-                      F.when((F.col('trx_lacci') != "-99") & (F.col('flag') == "y") & (F.col('area_name') != "UNKNOWN") & (F.col('find_flag') == (0)), (concat(lit('0#'),col('lacci_id_laccima'))))
-                        .when((F.col('trx_lacci') != "-99") & (F.col('flag') == "y") & (F.col('area_name') != "UNKNOWN") & (F.col('find_flag').isNotNull()), (concat(col('find_flag'),col('trx_lacci'))))
-                          .otherwise("-1#-99 "))
+                    F.when((F.col('trx_lacci') != "-99") & (F.col('flag') == "y") & (F.col('area_name') != "UNKNOWN") & (F.col('find_flag') == (0)), (concat(lit('0#'),col('lacci_id_laccima'))))
+                        .when((F.col('trx_lacci') != "-99") & (F.col('flag') == "y") & (F.col('area_name') != "UNKNOWN") & (F.col('find_flag').isNotNull()), (concat(col('find_flag'),col('trx_lacci'))))
+                            .otherwise("-1#-99 "))
 df1.show()
 
 # GET lacci_id and lacci_closing_flag from lacci_id_flag
 df_untilLACCIMA = df1.withColumn('lacci_closing_flag', split(col('lacci_id_flag'),'#').getItem(0))\
-                        .withColumn('lacci_id', split(col('lacci_id_flag'),'#').getItem(1))
+                        .withColumn('lacci_id', split(col('lacci_id_flag'),'#').getItem(1))
 
 # NVL
 df_untilLACCIMA = df_untilLACCIMA.fillna("NQ",["lac"]).fillna("NQ",["ci"]).fillna("NQ",["node_type"]).fillna("NQ",["area_name"]).fillna("NQ",["region_sales"]).fillna("NQ",["branch"]).fillna("NQ",["subbranch"]).fillna("NQ",["cluster_sales"]).fillna("NQ",["provinsi"]).fillna("NQ",["kabupaten"]).fillna("NQ",["kecamatan"]).fillna("NQ",["kelurahan"])
@@ -123,13 +123,13 @@ df_SAproduct = df_SAproduct.toDF('trx_date','rkey','mapping_key_type','service_t
 
 # SWITCH CASE rkey
 df_SAproduct = df_SAproduct.withColumn('rkey', 
-                      F.when((F.col('mapping_key_type').isNull()) | (F.col('mapping_key_type') == ""), F.col('rkey'))
-                          .otherwise(F.col('mapping_key_type')))
+                    F.when((F.col('mapping_key_type').isNull()) | (F.col('mapping_key_type') == ""), F.col('rkey'))
+                        .otherwise(F.col('mapping_key_type')))
 
 df_SAproduct = df_SAproduct.select('rkey','mapping_key_type','service_type','l1_name','l2_name','l3_name','l4_name','price','updated_date','product_name','plan_type','plan_service','quota_value','quota_unit','validity_value','validity_unit','vascode_type','file_id','load_ts','load_user')
 
 # JOIN ke SA_PRODUCT
-dfJOIN_SAproduct = df_untilLACCIMA.join(df_SAproduct,df_untilLACCIMA.offer_id ==  df_SAproduct.rkey, how = 'left')
+dfJOIN_SAproduct = df_untilLACCIMA.join(df_SAproduct,df_untilLACCIMA.offer_id == df_SAproduct.rkey, how = 'left')
 
 df_untilSAproduct = dfJOIN_SAproduct.select('payment_timestamp','trx_date','trx_hour','msisdn','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','payment_channel','cell_id','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','retry_count','subs_id','cust_type_desc','cust_subtype_desc','lacci_id_flag','lacci_id','lacci_closing_flag','area_name','lac','ci','node_type','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','l4_name','l3_name','l2_name','l1_name')
 
@@ -144,14 +144,14 @@ df_untilSAproduct = df_untilSAproduct.withColumn("file_id", lit("")).withColumn(
 df_untilSAproduct = df_untilSAproduct.select('payment_timestamp','trx_date','trx_hour','msisdn','subs_id','cust_type_desc','cust_subtype_desc','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','l4_name','l3_name','l2_name','l1_name','payment_channel','cell_id','lacci_id','lacci_closing_flag','lac','ci','node_type','area_name','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','file_id','load_ts','load_user','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10','filename','trx_lacci','lacci_id_flag','retry_count')
 
 # GET status_reject
-df_statusREJECT  = df_untilSAproduct.withColumn('status_reject', F.when((F.coalesce('subs_id','cust_type_desc','cust_subtype_desc','l4_name','l3_name','l2_name','l1_name','lacci_id','lacci_closing_flag','lac','ci','node_type','area_name','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan').isNull())\
-                                                                         | ((F.coalesce('subs_id','cust_type_desc','cust_subtype_desc','l4_name','l3_name','l2_name','l1_name','lacci_id','lacci_closing_flag','lac','ci','node_type','area_name','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan') == "NQ"))\
-                                                                            | ((F.coalesce('subs_id','cust_type_desc','cust_subtype_desc','l4_name','l3_name','l2_name','l1_name','lacci_id','lacci_closing_flag','lac','ci','node_type','area_name','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan') == "-99")), 'YES').otherwise('NO'))
+df_statusREJECT = df_untilSAproduct.withColumn('status_reject', F.when((F.coalesce('subs_id','cust_type_desc','cust_subtype_desc','l4_name','l3_name','l2_name','l1_name','lacci_id','lacci_closing_flag','lac','ci','node_type','area_name','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan').isNull())\
+                                                                | ((F.coalesce('subs_id','cust_type_desc','cust_subtype_desc','l4_name','l3_name','l2_name','l1_name','lacci_id','lacci_closing_flag','lac','ci','node_type','area_name','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan') == "NQ"))\
+                                                                    | ((F.coalesce('subs_id','cust_type_desc','cust_subtype_desc','l4_name','l3_name','l2_name','l1_name','lacci_id','lacci_closing_flag','lac','ci','node_type','area_name','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan') == "-99")), 'YES').otherwise('NO'))
 # CEK
 #print("Cek hasil status reject bervalue YES= ", df_statusREJECT.filter("status_reject =='YES'").show())
 
 # GET hasil GOOD dan REJECT
-df_hasilGOOD = df_statusREJECT.filter(col('status_reject')  == 'NO')
+df_hasilGOOD = df_statusREJECT.filter(col('status_reject') == 'NO')
 df_hasilGOOD = df_hasilGOOD.select('payment_timestamp','trx_date','trx_hour','msisdn','subs_id','cust_type_desc','cust_subtype_desc','bss_order_id','plan_id','plan_name','topping_id','topping_name','plan_price','offer_id','l4_name','l3_name','l2_name','l1_name','payment_channel','cell_id','lacci_id','lacci_closing_flag','lac','ci','node_type','area_name','region_sales','branch','subbranch','cluster_sales','provinsi','kabupaten','kecamatan','kelurahan','indicator_4g','future_string_1','future_string_2','future_string_3','brand','site_name','file_id','load_ts','load_user','pre_post_flag','event_date','bundling_id','future_string_5','future_string_6','future_string_7','future_string_8','future_string_9','future_string_10')
 
 df_hasilREJECT = df_statusREJECT.filter(col('status_reject') == 'YES')
